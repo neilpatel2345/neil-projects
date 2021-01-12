33410a17ecd90ee92d86d86dc955a258a88b922d
neilpatel2345 <77321219+neilpatel2345@users.noreply.github.com>
2021-01-12 03:03:08 -0500

    Add files via upload

---
+++ bankTransaction/bankInterface.java
@@ -0,0 +1,15 @@
+
+package bankTransaction;
+
+public interface bankInterface {
+   
+    public void accountUser();
+    public void accountPassword();
+    public long accountNumber();
+    public int accountPinNumber();
+    public void deposit();
+    public void withdrawl();
+    public void check();  
+    public String moneyInAccount();     
+    
+}
